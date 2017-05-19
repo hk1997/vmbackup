fd021f2129b0476eddff9f68a83e782c51ccefb1
hk1997 <khandelwal.hardik14@gmail.com>
2017-05-20 01:36:45 +0530

    first commit

--- controllers/process2.js
+++ controllers/process2.js
@@ -13,8 +13,8 @@ var path = require('path');
 //require('kue-scheduler') here
 var kue = require('kue-scheduler');
 var Queue = kue.createQueue();
-//mongoose.connect("mongodb://localhost/meanAuth");
-mongoose.connect('localhost','meanAuth');
+mongoose.connect("mongodb://localhost/meanAuth");
+
 //processing jobs
 Queue.process('schedule', function (job, done) {
   console.log('\nProcessing job with id %s at %s', job.id, new Date());
